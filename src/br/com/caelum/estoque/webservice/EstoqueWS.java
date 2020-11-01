99fb01dd2cd542201714579191d7cce1c72a64cd
William Foleis Mian <william_mian@yahoo.com.br>
2020-11-01 15:51:19 -0200

    estoquews - jax-ws - webservice - soap

---
+++ src/br/com/caelum/estoque/webservice/EstoqueWS.java
@@ -0,0 +1,21 @@
+package br.com.caelum.estoque.webservice;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.ItemDao;
+
+@WebService
+public class EstoqueWS {
+	
+	private ItemDao dao = new ItemDao();
+
+    public List<Item> getItens() {
+
+        System.out.println("Chamando getItens()");
+        return dao.todosItens();
+    }
+
+}
