ab32bcd172239e191e8bfa6a44b2a3d87a1028cd
William Mian <william_mian@yahoo.com.br>
2020-11-10 17:20:02 -0300

    ResponseWrapper RequestWrapper

--- src/br/com/caelum/estoque/webservice/EstoqueWS.java
+++ src/br/com/caelum/estoque/webservice/EstoqueWS.java
@@ -3,8 +3,10 @@ package br.com.caelum.estoque.webservice;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.ResponseWrapper;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -18,8 +20,10 @@ public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 
 	@WebMethod(operationName = "todosOsItens")
+	@ResponseWrapper(localName = "itens")
 	@WebResult(name = "itens")
-    public ListaItens getItens(Filtros filtros) {
+	//@RequestWrapper(localName = "listaItens")
+    public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
         System.out.println("Chamando getItens()");
         List<Filtro> lista = filtros.getLista();
         List<Item> itensResultado = dao.todosItens(lista);
