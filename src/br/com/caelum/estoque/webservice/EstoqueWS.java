de930dc517713e3c44b791b3d43b95e411331cab
William Foleis Mian <william_mian@yahoo.com.br>
2020-11-10 00:50:00 -0200

    filtrando resultados

--- src/br/com/caelum/estoque/webservice/EstoqueWS.java
+++ src/br/com/caelum/estoque/webservice/EstoqueWS.java
@@ -1,9 +1,14 @@
 package br.com.caelum.estoque.webservice;
 
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
+import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
 
@@ -14,9 +19,11 @@ public class EstoqueWS {
 
 	@WebMethod(operationName = "todosOsItens")
 	@WebResult(name = "itens")
-    public ListaItens getItens() {
+    public ListaItens getItens(Filtros filtros) {
         System.out.println("Chamando getItens()");
-        return new ListaItens(dao.todosItens());
+        List<Filtro> lista = filtros.getLista();
+        List<Item> itensResultado = dao.todosItens(lista);
+        return new ListaItens(itensResultado);
     }
 
 }
