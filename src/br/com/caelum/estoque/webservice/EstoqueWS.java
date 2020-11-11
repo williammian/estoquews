9ebdb512cb42e0fa6eb88e2eb4d0b28ae0d660de
William Foleis Mian <william_mian@yahoo.com.br>
2020-11-11 00:20:19 -0200

    cadastrarItem

--- src/br/com/caelum/estoque/webservice/EstoqueWS.java
+++ src/br/com/caelum/estoque/webservice/EstoqueWS.java
@@ -6,7 +6,6 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.ResponseWrapper;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -20,7 +19,7 @@ public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 
 	@WebMethod(operationName = "todosOsItens")
-	@ResponseWrapper(localName = "itens")
+	//@ResponseWrapper(localName = "itens")
 	@WebResult(name = "itens")
 	//@RequestWrapper(localName = "listaItens")
     public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
@@ -29,5 +28,13 @@ public class EstoqueWS {
         List<Item> itensResultado = dao.todosItens(lista);
         return new ListaItens(itensResultado);
     }
+	
+	@WebMethod(operationName="CadastrarItem") 
+	@WebResult(name="item")
+	public Item cadastrarItem(@WebParam(name="item") Item item) {
+	  System.out.println("Cadastrando " + item);
+	  this.dao.cadastrar(item);
+	  return item;
+	}
 
 }
